38af42cb416a848e01311fd2426e4762f13cac3e
Orest Polishchuk <73292186+polishchukorest@users.noreply.github.com>
2022-09-07 13:29:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import csv
 import numpy as np
-import sys
 from lightmatchingengine.lightmatchingengine import LightMatchingEngine, Side
 
-from threading import Thread
-from collections import deque
 
 
+#simple converting function
 def convert_side(str):
     if str.lower().strip() in ['bid', 'buy']:
         return 1
@@ -18,25 +16,27 @@ def convert_side(str):
 
 
 if __name__ == "__main__":
-
+    #reading file with order trades
     df = pd.read_csv("TestTask//okcoin_incremental_book_L2_2020-01-01_BTC-USD_raw.csv")
     df.drop('is_snapshot', axis=1, inplace=True)
 
+    #reading file with market trades
     df_market = pd.read_csv("TestTask//okcoin_trades_2020-01-01_BTC-USD.csv", delimiter=';')
+    #converting them to the order trades with min possible price for sell and max pirce for buy
     df_market['price'] = np.where(df_market['side'] == 'sell', np.finfo(np.float64).min, np.finfo(np.float64).max)
     df_market = df_market.loc[:, df.columns]
+    #adding them in one dataframe and sorting them by time
     df = df.append(df_market)
     df.sort_values(by=['timestamp', 'local_timestamp'])
     print(df.size)
 
     lme = LightMatchingEngine()
 
-    trades_dict = dict(order_id=[], instmt=[], trade_price=[], trade_qty=[], trade_side=[], trade_id=[])
-    trades_df = pd.DataFrame(trades_dict)
     # trades_df = pd.DataFrame()
     file = open('trades.csv', 'w')
     writer = csv.writer(file)
 
+    #iterating over dataframe, taking data from each row and adding to the matching machine
     for index, row in df.iterrows():
         print(index)
         name = row['symbol']
@@ -47,22 +47,10 @@ if __name__ == "__main__":
         order, trades = lme.add_order(name, price, qty, side)
         if trades is not None:
             for trade in trades:
+                #writing directly to csv for speedup
                 writer.writerow([trade.order_id, trade.instmt,
                                  trade.trade_price, trade.trade_qty,
                                  trade.trade_side, trade.trade_id])
-                '''trades_df = trades_df.append([trade.order_id, trade.instmt,
-                                  trade.trade_price, trade.trade_qty,
-                                  trade.trade_side, trade.trade_id])'''
-                # print(trades_df.size)
-                '''trades_dict['order_id'].append(trade.order_id)
-                trades_dict['instmt'].append(trade.instmt)
-                trades_dict['trade_price'].append(trade.trade_price)
-                trades_dict['trade_qty'].append(trade.trade_qty)
-                trades_dict['trade_side'].append(trade.trade_side)
-                trades_dict['trade_id'].append(trade.trade_id)'''
-        # if index == 1000:
-        #    break
+
     file.close()
-    # trades_df.to_csv('trades.csv')
-    # print(len(trades))
-    # print(trades_dict.items())
+
